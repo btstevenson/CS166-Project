068e2b27a730404acb9463341659c7a737831acc
Brandon Stevenson <bstev002@well.cs.ucr.edu>
2015-05-24 18:15:14 -0700

    initial commit of project files which include csv files from excel file

---
+++ sql/scripts/create_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+psql -p $PGPORT project < $DIR/../src/create_tables.sql
+psql -p $PGPORT project < $DIR/../src/create_index.sql
+psql -p $PGPORT project < $DIR/../src/load_data.sql
---
+++ sql/src/load_data.sql
@@ -0,0 +1,5 @@
+COPY USR FROM '/home/csmajs/bstev002/database/Files/CS166_Project/data/Users.csv' WITH DELIMITER ',' CSV HEADER;
+COPY WORK_EXPR FROM '/home/csmajs/bstev002/database/Files/CS166_Project/data/Work_Ex.csv' WITH DELIMITER ',' CSV HEADER;
+COPY EDUCATIONAL_DETAILS FROM '/home/csmajs/bstev002/database/Files/CS166_Project/data/Edu_Det.csv' WITH DELIMITER ',' CSV HEADER;
+COPY MESSAGE FROM '/home/csmajs/bstev002/database/Files/CS166_Project/data/Messages.csv' WITH DELIMITER ',' CSV HEADER;
+COPY CONNECTION_USR FROM '/home/csmajs/bstev002/database/Files/CS166_Project/data/Connection.csv' WITH DELIMITER ',' CSV HEADER;
